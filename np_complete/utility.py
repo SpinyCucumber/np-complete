4258b40d019d2a1491c90a0f7c30cf21203524da
Elijah Hilty <spinycucumber@gmail.com>
2023-05-01 21:36:18 -0500

    More friendly file utility functions

--- np_complete/utility.py
+++ np_complete/utility.py
@@ -3,28 +3,57 @@ from sympy import Symbol
 from sympy.logic import Not
 from .definitions import SubsetSum, Partition, Sat
 
-def read_lines(io: TextIO) -> Generator[str, None, None]:
-    while True:
-        line = io.readline()
-        if line == "": break # Detect EOF
-        line = line[:-1] # Strip trailing newline
-        if line == "$": break # Detect $
-        yield line
+FileOrPath = Union[TextIO, str]
 
-def read_subset_sum(io: TextIO) -> SubsetSum:
-    lines = read_lines(io)
+def read_lines(file_or_path: FileOrPath) -> Generator[str, None, None]:
+    """
+    Yields each line of a file until encountering a line with a single '$' character or EOF
+
+    Accepts either a file object or a path, which will be opened as a file object
+    """
+    # Convert to file if necessary
+    if isinstance(file_or_path, str):
+        with open(file_or_path, "r") as file:
+            yield from read_lines(file)
+    else:
+        file = file_or_path
+        while True:
+            line = file.readline()
+            if line == "": break # Detect EOF
+            line = line[:-1] # Strip trailing newline
+            if line == "$": break # Detect $
+            yield line
+
+def read_subset_sum(file_or_path: FileOrPath) -> SubsetSum:
+    """
+    Parses an instance of the subset sum problem from a file
+
+    The first line defines the target sum, while each consecutive line defines a number in the set.
+    Accepts either a file obejct or a path.
+    """
+
+    lines = read_lines(file_or_path)
     target = int(next(lines)) # Read target sum
     numbers = [int(line) for line in lines] # Read remaining numbers
     return SubsetSum(target, numbers)
 
-def read_partition(io: TextIO) -> Partition:
-    numbers = [int(line) for line in read_lines(io)]
+def read_partition(file_or_path: FileOrPath) -> Partition:
+    """
+    Parses an instance of the partition problem from a file
+
+    Each line defines a number in the set.
+    Accepts either a file obejct or a path.
+    """
+
+    numbers = [int(line) for line in read_lines(file_or_path)]
     return Partition(numbers)
 
 def write_sat(io: TextIO, sat: Sat, to_index: Callable[[Symbol], int]):
     """
     Seralizes an instance of the SAT problem and writes it to a file
-    Must provide a mapping between symbol and index
+
+    Must provide a mapping between symbol and index, as variables are represented internally as symbols
+    but must be serialized as indicies.
     """
 
     def serialize_literal(literal: Union[Symbol, Not]) -> str:
@@ -40,6 +69,3 @@ def write_sat(io: TextIO, sat: Sat, to_index: Callable[[Symbol], int]):
     # Output each clause as a line
     for clause in sat.expression.args:
         print(" ".join(map(serialize_literal, clause.args)), file=io)
-    
-    # Output $
-    print("$", file=io)
